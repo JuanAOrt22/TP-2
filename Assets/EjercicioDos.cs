9a8adf8988984270bd84f3b5c5e32b1895dde9ef
ATAEFE JUAN ARIEL <46956717@ort>
2022-04-26 14:32:00 -0300

    Initial commit

---
+++ Assets/EjercicioDos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EjercicioDos : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text eso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+            eso.text = "Es mayor de edad";
+        }
+
+        else
+        {
+            Debug.Log("No es mayor de edad");
+            eso.text = "Es mayor de edad";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
