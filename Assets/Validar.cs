8c10239c5aab2c7daae0dba19d65a65877029d4d
JuanAOrt22 <101871735+JuanAOrt22@users.noreply.github.com>
2022-04-26 22:28:12 -0300

    tp
    a

---
+++ Assets/Validar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Validar : MonoBehaviour
+{
+    string contraseña;
+    string contraseñausuario;
+    public Text Ingresousuario;
+    public GameObject cartelitomsj;
+    public Text textomsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseña = "12345";
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validar()
+    {
+        contraseñausuario = Ingresousuario.text;
+        if(contraseñausuario == contraseña)
+        {
+            cartelitomsj.SetActive(true);
+            Debug.Log("Bienvenido.");
+            textomsj.text = "Bienvenido.";
+        }
+        else
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Contraseña Incorrecta.";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
